04fa2d113d1c5fa084f750a2469b53ec1e84b2bf
unknown <ryooo@.(none)>
2011-10-04 01:01:12 +0900

    first commit!

---
+++ json/registed_tweets.js
@@ -0,0 +1,50 @@
+set_messages([
+{"nick_name": "hoge",
+"user_attr":"（男性／20代／東京都）",
+"user_url":"http://hogehogehoge.co.jp/",
+"user_image":"img/image_bigger.jpg",
+"message":"神奈川で資材運搬のヘリが墜落・炎上 機長が死亡 - テレビ朝日"
+},
+{"nick_name": "hoge♪",
+"user_attr":"（女性／30代／愛知県）",
+"user_url":"http://hogehogehoge.co.jp/",
+"user_image":"img/image_bigger.jpg",
+"message":"ノーベル賞受賞スタインマン氏死亡していた 発表数日前 - 朝日新聞"
+},
+{"nick_name": "hoge☆",
+"user_attr":"（男性／大阪府）",
+"user_url":"http://hogehogehoge.co.jp/",
+"user_image":"img/image_bigger.jpg",
+"message":"被災地の学生も出席＝大手企業が内定式 - ウォール・ストリート・ジャーナル日本版"
+},
+{"nick_name": "hoge▼",
+"user_attr":"（男性／40代／宮崎県）",
+"user_url":"http://hogehogehoge.co.jp/",
+"user_image":"img/image_bigger.jpg",
+"message":"公務員宿舎：「朝霞」建設５年凍結 都内３区は原則廃止 - 毎日新聞"
+},
+{"nick_name": "hpge○",
+"user_attr":"（女性／10代／兵庫県）",
+"user_url":"http://hogehogehoge.co.jp/",
+"user_image":"img/image_bigger.jpg",
+"message":"失言を警戒？首相「ぶら下がり取材」取りやめ - 読売新聞"
+},
+{"nick_name": "hpgeっ",
+"user_attr":"（女性／東京都）",
+"user_url":"http://hogehogehoge.co.jp/",
+"user_image":"img/image_bigger.jpg",
+"message":"マンション汚水槽から人骨 東京・東久留米、死後半年超 - 朝日新聞"
+},
+{"nick_name": "hpge！",
+"user_attr":"（男性）",
+"user_url":"http://hogehogehoge.co.jp/",
+"user_image":"img/image_bigger.jpg",
+"message":"溶血性尿毒症症候群で３歳園児死亡 各務原 - 中日新聞"
+},
+{"nick_name": "hpge？",
+"user_attr":"（30代）",
+"user_url":"http://hogehogehoge.co.jp/",
+"user_image":"img/image_bigger.jpg",
+"message":"比の鉱山に武装勢力 住友鉱山、日本人社員の無事確認 - 日本経済新聞"
+}
+])
